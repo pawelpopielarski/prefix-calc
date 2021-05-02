146298170e4dd0e439e7bc23e1401fd003239994
pawel.popielarski <pawel@popielarski.pl>
2021-05-02 12:40:19 +0200

    bonus service

---
+++ CalcService.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, make_response, jsonify
+
+import PrefixCalculator, InfixCalculator
+import calcerrors
+
+app = Flask(__name__)
+
+def handleCalc(req, calc):
+    try:
+        result = calc.calculate(req.json['statement'].split())
+        return make_response(jsonify({'result': '{}'.format(result)}), 200)
+    except(calcerrors.InvalidInputError) as e:
+        return make_response(jsonify({'error': '{}'.format(str(e))}), 400)
+
+def validate(r):
+    if not r.data or not r.json:
+        return False, make_response(jsonify({'error': 'no json'}), 400)
+    if not 'statement' in r.json:
+        return False, make_response(jsonify({'error': 'statement field missing in json'}), 400)
+    return True, None
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/')
+def index():
+    return "GET /calc/v1 for detailed API description"
+
+@app.route('/calc/v1', methods=['GET'])
+def help():
+    return "POST /calc/v1/infix\nPOST /calc/v1/prefix"
+
+@app.route('/calc/v1/infix', methods=['POST'])
+def infix():
+    valid, reply = validate(request)
+    if not valid:
+        return reply
+    return handleCalc(request, InfixCalculator.InfixCalculator())
+
+@app.route('/calc/v1/prefix', methods=['POST'])
+def prefix():
+    valid, reply = validate(request)
+    if not valid:
+        return reply
+    return handleCalc(request, PrefixCalculator.PrefixCalculator())
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # prefix-calc
 
-Done with python 3.8.5
+Done with python 3.8.5<br>
+Done with Flask
 
-Part 1 - PrefixCalculator.py.<br>
-Part 2 - InfixCalculator.py.<br>
-Bonus - web-based version of the calculator.<br>
+Part 1 - PrefixCalculator.py<br>
+Part 2 - InfixCalculator.py<br>
+
+Bonus -  CalcService.py<br>
+curl -H "Content-Type: application/json" -X POST <url>/calc/v1/infix -d '{"statement":"( 1 + ( 2 * 3 ) )"}'<br>
+curl -H "Content-Type: application/json" -X POST <url>/calc/v1/prefix -d '{"statement":"- / 10 + 1 1 * 1 2"}'<br>
